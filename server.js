67c1660d2906167515486be05966ae7e20924475
Siris123 <sp9616391919@gmail.com>
2024-03-09 22:40:35 +0545

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) =>{
     res.send('hello from server');
 })
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
