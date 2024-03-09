cb6a8a583b37b7077c0cc6ddb712042d0ea82142
Siris123 <sp9616391919@gmail.com>
2024-03-10 00:21:08 +0545

    setup

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`app is listening on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("error:", error);
+    throw err;
+  }
+})();
+*/
