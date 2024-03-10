fc703276b53b688384ee098cf9f7479082ade6d0
Siris123 <sp9616391919@gmail.com>
2024-03-10 11:24:50 +0545

    setup of errors handling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db Connection Failed !!!", err);
+  });
 
 
 
 
+
+
+  
 /*
 import express from "express";
 const app = express();
